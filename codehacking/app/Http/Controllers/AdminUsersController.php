13234653528a61ced5de5244e7605d3d620a42d5
phpvytautas <csvytas@gmail.com>
2018-03-29 13:05:05 +0300

    users page working

--- codehacking/app/Http/Controllers/AdminUsersController.php
+++ codehacking/app/Http/Controllers/AdminUsersController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
+use App\Http\Requests\UsersRequest;
 
 class AdminUsersController extends Controller
 {
@@ -14,7 +17,10 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+
+        $users = User::all();
+
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -25,7 +31,10 @@ class AdminUsersController extends Controller
     public function create()
     {
 
-        return view('admin.users.create');
+
+        $roles = Role::pluck('name', 'id')->all();
+
+        return view('admin.users.create', compact('roles'));
         //
     }
 
@@ -35,9 +44,12 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
         //
+        User::create($request->all());
+
+        return redirect('/admin/users');
     }
 
     /**
--- codehacking/resources/views/admin/users/index.blade.php
+++ codehacking/resources/views/admin/users/index.blade.php
@@ -1,5 +1,48 @@
-<h1>
+@extends('layouts.admin')
 
-    Its working
 
-</h1>
+
+@section('content')
+
+<h1>Users</h1>
+
+
+<div class="container">
+    <h2>Basic Table</h2>
+    <p>The .table class adds basic styling (light padding and only horizontal dividers) to a table:</p>            
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Role</th>
+          <th>Status</th>
+          <th>Created</th>
+          <th>Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+       
+        @if($users)
+
+            @foreach($users as $user)
+
+        <tr>
+          <td>{{$user->id}}</td>
+          <td>{{$user->name}}</td>
+          <td>{{$user->email}}</td>
+          <td>{{data_get($user, 'role.name')}} </td>
+          <td>{{$user->is_active == 1 ? 'Active' : 'Not Active'}}</td>
+          <td>{{$user->created_at->diffForHumans()}}</td>
+          <td>{{$user->updated_at->diffForHumans()}}</td>
+        </tr>
+            @endforeach
+
+        @endif
+
+      </tbody>
+    </table>
+  </div>
+
+@stop
